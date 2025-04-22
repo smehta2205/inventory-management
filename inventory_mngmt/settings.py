32e1930d930037c16a8703df635216066fd1d20b
Stuti Mehta <stutimehta97@gmail.com>
2025-04-22 13:21:31 -0500

    Update setting and set debug to false

--- inventory_mngmt/settings.py
+++ inventory_mngmt/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-md_2fk0bqcn)@ag-g_hq53+gx%o+4mfsmt7k+&8*6x9uq+prsg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 LOGIN_REDIRECT_URL = '/'
