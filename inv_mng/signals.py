94f6a17d70f0aba95ac569a061cccf1bd667c8b6
Stuti Mehta <stutimehta97@gmail.com>
2025-05-06 12:42:04 -0500

    Update user to customuser in signals

--- inv_mng/signals.py
+++ inv_mng/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Stock, Notification, Item
+# from django.contrib.auth.models import User
+from .models import Stock, Notification, Item, CustomUser
 
 @receiver(post_save, sender=Stock)
 def check_stock_threshold(sender, instance, **kwargs):
@@ -14,7 +14,7 @@ def check_stock_threshold(sender, instance, **kwargs):
     print(min_threshold)
 
     if instance.total_quantity < min_threshold:
-        staff_users = User.objects.filter(is_staff=True)
+        staff_users = CustomUser.objects.filter(is_staff=True)
         for user in staff_users:
             Notification.objects.get_or_create(
                 user=user,
